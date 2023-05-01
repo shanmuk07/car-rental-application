a98773011d86c2bea94e499a654fd471d49cbb7b
shanmukh07 <m.shanmukh07@gmail.com>
2023-05-01 19:01:30 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
+import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Userhome from './usercomponents/Userhome';
+import Userstatus from './usercomponents/Userstatus'; 
+import Userhistory from './usercomponents/Userhistory';
+
+
+import Car1 from './usercomponents/car1';
+import Car2 from './usercomponents/car2';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Userhome/>}/>
+      
+      <Route path='/Userhistory' element={<Userhistory/>}/>
+      
+      <Route path='/Userstatus' element={<Userstatus/>}/>
+      <Route path='/car1' element={<Car1/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import Ownerapp from './Ownerapp';
+import Adminapp from './Adminapp';
+import Signup from './Signup';
+import Log1 from './Log1'
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <Ownerapp/> */}
+    {/* <Adminapp/> */}
+    {/* <Signup/> */}
+    {/* <Log1/> */}
+    
+  
   </React.StrictMode>
 );
 
